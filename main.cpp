c979d131df0af28f8862802d0f56e1d485268c80
Joshua Mac <joshua160597@googlemail.com>
2024-01-22 22:12:01 +0100

    22/01/2024

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include <cstdint>
 
+float inputValues()
+{
+    float ValueA = 0;
+    float ValueB = 0;
+    std::cout << "Bitte gib den ersten Wert ein:" << "\n";
+    std::cin >> ValueA;
+    std::cout << "Bitte gib den zweiten Wert ein:" << "\n";
+    std::cin >> ValueB;
+
+    return ValueA,ValueB;
+}
+
+void operation()
+{
+    inputValues()
+}
+
 int main()
 {
     std::cout << "Hallo Welt!" << std::endl;
