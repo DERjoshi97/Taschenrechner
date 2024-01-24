33f7397825d4b07379327d21a5cda07646be7dab
Joshua Mac <joshua160597@googlemail.com>
2024-01-24 21:42:26 +0100

    Taschenrechner V 0.1/24/01/2024

--- main.cpp
+++ main.cpp
@@ -1,26 +1,72 @@
 #include <iostream>
 #include <cstdint>
+#include <string>
 
-float inputValues()
+float InputUserValue()
 {
-    float ValueA = 0;
-    float ValueB = 0;
-    std::cout << "Bitte gib den ersten Wert ein:" << "\n";
-    std::cin >> ValueA;
-    std::cout << "Bitte gib den zweiten Wert ein:" << "\n";
-    std::cin >> ValueB;
-
-    return ValueA,ValueB;
+    std::cout << " Eingabe einer Zahl:" << "\n";
+    float entry;
+    std::cin >> entry;
+    return entry;
 }
-
-void operation()
+int InputUserOperator()
 {
-    inputValues()
+    std:: cout << "Please enter the operator (Addition [1], Substraction[2], Multiplication[3], Division[4])" << "\n";
+    int entry;
+    std::cin >> entry;
+    return entry;
 }
+enum class UsedOperators
+{
+    Addition = 1,
+    Substraction = 2,
+    Multiplication = 3,
+    Division = 4
+};
 
+float operation(float ValueA, float ValueB, auto Operator)
+{
+    switch (Operator)
+    {
+    case UsedOperators::Addition:
+        {
+            float operatedValue = ValueA + ValueB;
+            return operatedValue;
+        }
+        break;
+     case UsedOperators::Substraction:
+        {
+            float operatedValue = ValueA - ValueB;
+            return operatedValue;
+        }
+        break;
+     case UsedOperators::Multiplication:
+        {
+            float operatedValue = ValueA * ValueB;
+            return operatedValue;
+        }
+        break;
+      case UsedOperators::Division:
+        {
+            float operatedValue = ValueA / ValueB;
+            return operatedValue;
+        }
+        break;
+    default:
+        {
+            std::cout << "No valid operand!";
+        }
+        break;
+    }
+}
 int main()
 {
-    std::cout << "Hallo Welt!" << std::endl;
+    float ValueA = InputUserValue();
+    float ValueB = InputUserValue();
+    int Operator = InputUserOperator();
+    auto transformedOperator = static_cast <UsedOperators>(Operator);
+    float Ergebnis = operation(ValueA, ValueB, transformedOperator);
 
+    std::cout << Ergebnis << "\n";
     return 0;
 }
